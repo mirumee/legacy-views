0211c79b24e3e2054b5d6dc09f5b8fc21d6c17c1
Mira Szewczyk <szewczyk.mira@gmail.com>
2019-11-21 14:21:16 +0100

    Split classmethod into two

--- saleor/graphql/account/i18n.py
+++ saleor/graphql/account/i18n.py
@@ -12,7 +12,7 @@ class I18nMixin:
     """
 
     @classmethod
-    def validate_address(cls, address_data: dict, instance=None):
+    def validate_address_form(cls, address_data: dict, instance=None):
         phone = address_data.get("phone", None)
         if phone:
             try:
@@ -26,11 +26,16 @@ class I18nMixin:
                     }
                 ) from exc
 
-        address_form, _ = get_address_form(address_data, address_data.get("country"))
-
+        address_form, _ = get_address_form(
+            address_data, address_data.get("country"), instance=instance
+        )
         if not address_form.is_valid():
             raise ValidationError(address_form.errors.as_data())
+        return address_form
 
+    @classmethod
+    def validate_address(cls, address_data: dict, instance=None):
+        address_form = cls.validate_address_form(address_data)
         if not instance:
             instance = Address()
 
