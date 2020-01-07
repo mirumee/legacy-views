dba766a3d635dc22b7bbc54ce17817c05788f536
Mira Szewczyk <szewczyk.mira@gmail.com>
2020-01-07 10:12:16 +0100

    Update schama fields in tests

--- tests/api/test_product.py
+++ tests/api/test_product.py
@@ -150,7 +150,7 @@ def test_product_query(staff_api_client, product, permission_manage_products, st
                         variants {
                             name
                         }
-                        isAvailableInCountry
+                        isAvailable
                         pricing {
                             available,
                             priceRange {
@@ -206,7 +206,7 @@ def test_product_query(staff_api_client, product, permission_manage_products, st
     purchase_cost, margin = get_product_costs_data(product)
     assert purchase_cost.start.amount == product_data["purchaseCost"]["start"]["amount"]
     assert purchase_cost.stop.amount == product_data["purchaseCost"]["stop"]["amount"]
-    assert product_data["isAvailableInCountry"] is product.is_visible
+    assert product_data["isAvailable"] is product.is_visible
     assert product_data["pricing"]["available"] is product.is_visible
     assert margin[0] == product_data["margin"]["start"]
     assert margin[1] == product_data["margin"]["stop"]
